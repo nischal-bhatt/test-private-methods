aea25f1a78ee881924583cb948091a4a9820be1e
nischal-bhatt <nischal.bhatt.87@gmail.com>
2023-02-12 22:44:34 +0800

    asd

--- src/main/java/test/pvt/methods/Adder.java
+++ src/main/java/test/pvt/methods/Adder.java
@@ -2,6 +2,14 @@ package test.pvt.methods;
 
 public class Adder {
 
+	public static void main(String[] args)
+	{
+		System.out.println("egt");
+		Adder a = new Adder();
+		System.out.println(a.add(2, 3, 4));
+		System.out.println();
+	}
+	
 	private int add(Integer a, Integer b, Integer c)
 	{
 		System.out.println("we are inside here , but from outside! ");
